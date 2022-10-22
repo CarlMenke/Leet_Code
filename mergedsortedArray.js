98d025cffe23a037a51b879bf0a583038cf7c31f
carlmenke <carlmenke13@gmail.com>
2022-10-22 09:32:07 -0700

    sorted the single array

--- mergedsortedArray.js
+++ mergedsortedArray.js
@@ -1,12 +1,14 @@
 var merge = function (nums1, m, nums2, n) {
-  for (let i = 0; i < nums1.length; i++) {
-    //replace the 0's in nums1 with the numbers of nums2
-    //run sort on the array
-    for (let i = 0; i < n; i++) {
-      nums1.splice(nums1.indexOf(0), 1, nums2[i]);
-    }
-    console.log(nums1);
+  //replace the 0's in nums1 with the numbers of nums2
+  //run sort on the array
+  for (let i = 0; i < n; i++) {
+    nums1.splice(nums1.indexOf(0), 1, nums2[i]);
   }
+
+  nums1.sort((a, b) => {
+    return a - b;
+  });
+  console.log(nums1);
 };
 
 merge([2, 0], 1, [1], 1);
